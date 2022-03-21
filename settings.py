ea05c4048e468a280453b530820a9dc63458dc58
Abdulazeez Olabode <azeezolabode010@gmail.com>
2022-03-21 19:07:44 +0100

    stress

--- settings.py
+++ settings.py
@@ -28,7 +28,9 @@ for row in csv_list:
 
 def check_currency(ip):
     try:
+        print("IP addr", ip)
         response = reader.country(ip)
+        print("Currency", response.country.iso_code)
         currency_spent = CURRENCIES.get(response.country.iso_code)
         if currency_spent not in ACCEPTED_CURRENCIES:
             currency_spent = "USD"
